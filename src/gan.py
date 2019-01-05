f229b05380998d76df8abe35755bcea8b1380c66
ZijingPeng <zjpeng789@outlook.com>
2019-01-05 11:05:30 +0800

    Revise network structure
    Add U-Net structure

--- src/gan.py
+++ src/gan.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 
 def generator(z, x, output_dim, reuse=False, alpha=0.2, training=True):
     with tf.variable_scope('generator', reuse=reuse):
-
-        x1 = tf.layers.conv2d(x, 64, 4, strides=4, padding='same')
+        x1 = tf.layers.conv2d(x, 64, 4, strides=2, padding='same')
         relu1 = tf.maximum(alpha * x1, x1)
         # 64x64x64
 
@@ -41,7 +40,6 @@ def generator(z, x, output_dim, reuse=False, alpha=0.2, training=True):
         relu7 = tf.reshape(relu7, (-1, 1, 1, 1, 512))
 
         nz = tf.layers.dense(z, 512)
-
         # Reshape it to start the convolutional stack
         xz = tf.reshape(nz, (-1, 1, 1, 1, 512))
         relu7 += xz
@@ -49,32 +47,42 @@ def generator(z, x, output_dim, reuse=False, alpha=0.2, training=True):
         x8 = tf.layers.conv3d_transpose(relu7, 256, 4, strides=2, padding='same')
         bn8 = tf.layers.batch_normalization(x8, training=training)
         relu8 = tf.maximum(alpha * bn8, bn8)
-        # 2x2x2x512
+        # 2x2x2x256
+
+        relu8 = tf.concat([relu8, tf.reshape(relu6, (-1, 2, 2, 2, 256))], 4)
+        # relu8 = tf.concat([relu8, relu1], 4)
 
         x9 = tf.layers.conv3d_transpose(relu8, 128, 4, strides=2, padding='same')
         bn9 = tf.layers.batch_normalization(x9, training=training)
         relu9 = tf.maximum(alpha * bn9, bn9)
         # 4x4x4x128
 
+        relu9 = tf.concat([relu9, tf.reshape(relu5, (-1, 4, 4, 4, 128))], 4)
+
         x10 = tf.layers.conv3d_transpose(relu9, 64, 4, strides=2, padding='same')
         bn10 = tf.layers.batch_normalization(x10, training=training)
         relu10 = tf.maximum(alpha * bn10, bn10)
         # 8x8x8x64
+
+        relu10 = tf.concat([relu10, tf.reshape(relu4, (-1, 8, 8, 8, 64))], 4)
 
         x11 = tf.layers.conv3d_transpose(relu10, 32, 4, strides=2, padding='same')
         bn11 = tf.layers.batch_normalization(x11, training=training)
         relu11 = tf.maximum(alpha * bn11, bn11)
         # 16x16x16x32
 
+        relu11 = tf.concat([relu11, tf.reshape(relu3, (-1, 16, 16, 16, 16))], 4)
+
         x12 = tf.layers.conv3d_transpose(relu11, 16, 4, strides=2, padding='same')
         bn12 = tf.layers.batch_normalization(x12, training=training)
         relu12 = tf.maximum(alpha * bn12, bn12)
         # 32x32x32x16
 
-        logits = tf.layers.conv3d_transpose(relu12, output_dim, 4, strides=1, padding='same')
+        logits = tf.layers.conv3d_transpose(relu12, 1, 4, strides=1, padding='same')
 
         out = tf.tanh(logits)
         return out
+
 
 
 def discriminator(xv, xp, reuse=False, alpha=0.2):
